ce871b3602d77152edf73081a9ea7e6063cac862
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2021-12-13 10:41:41 +0800

    Change the Email Intent function in about.kt

--- app/src/main/java/com/example/about/about.kt
+++ app/src/main/java/com/example/about/about.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class about : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +19,24 @@ class about : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         eee.setOnClickListener() {
-            val intent = Intent(this, com.example.about.emailc::class.java)
-            startActivity(intent)
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.setType("text/plain")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, "wecare@gmail.com")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "WeCare Apps")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "We are here for you. ")
+
+            startActivity(Intent.createChooser(emailIntent, "Send Email"))
+
+            if (emailIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(emailIntent)
+            } else {
+                Toast.makeText(this, "There is no application that support this action",
+                    Toast.LENGTH_SHORT).show();
+            }
         }
+            /*startActivity(Intent.createChooser(intent, "Send Email"))
+            val intent = Intent(this, com.example.about.emailc::class.java)
+            startActivity(intent)*/
 
         ttt.setOnClickListener(){
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.termsofusegenerator.net/live.php?token=56SWDt8rPJZrv6KP8OEfFwDQQsMdw9d3"))
